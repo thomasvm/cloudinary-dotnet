4977213ac2ec6035a910ec867760188b934c6dda
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-04-08 20:20:21 +0200

    Some initial classes, nothing real yet

---
+++ src/Cloudinary/AccountConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cloudinary
+{
+    public class AccountConfiguration
+    {
+        public string CloudName { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string ApiSecret { get; set; }
+    }
+}
---
+++ src/Cloudinary/Transformation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cloudinary
+{
+    public class Transformation
+    {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public CropMode? Crop { get; set; }
+
+        public Transformation(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Crop = null;
+        }
+    }
+}
