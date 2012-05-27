47d3925d67827d50b735aab41a280ad871004414
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-05-27 14:03:11 +0200

    Create a shared assemblyinfo file

---
+++ src/Cloudinary.CommandLine/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("Cloudinary")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+// Version info
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
