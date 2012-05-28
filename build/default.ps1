2853eeaf4d230d9bb83eb92cfd598876029bc63d
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-05-28 15:50:39 +0200

    Prepare

--- build/default.ps1
+++ build/default.ps1
@@ -8,3 +8,7 @@ task Build {
     Exec { msbuild /p:Configuration=$mode ../src/Cloudinary.sln }
 }
 
+task Test -depends Build {
+    Exec { .\nunit\nunit-console.exe ..\src\Cloudinary.Tests\bin\$mode\Cloudinary.Tests.dll /framework=4.0 }
+}
+
