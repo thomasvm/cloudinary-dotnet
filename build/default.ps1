53af6a44785a999896f4be6aee5980966b44cb23
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-05-29 12:49:47 +0200

    Add package step to build script

--- build/default.ps1
+++ build/default.ps1
@@ -2,6 +2,8 @@ properties {
     $mode = "Debug"
 }
 
+$config = New-Object PSObject -property @{ version="0.0.1";releaseNotes="Initial release" }
+
 task default -depends Build
 
 task Build {
@@ -11,5 +13,35 @@ task Build {
 task Test -depends Build {
     Exec { .\nunit\nunit-console.exe ..\src\Cloudinary.Tests\bin\$mode\Cloudinary.Tests.dll }
     Remove-Item TestResult.xml
+}
+
+task Package {
+    # Force release build
+    $mode = "Release"
+    Invoke-Task Build
+    Invoke-Task Test
+
+    # Create pkg
+    If(Test-Path pkg) {
+        Remove-Item -Recurse pkg
+    }
+    New-Item -type directory pkg
+    New-Item -type directory pkg\lib
+    Copy-Item ..\src\Cloudinary\bin\$mode\Cloudinary.dll pkg\lib
+
+    SetNuSpecVersionInfo
+
+    Exec {
+        ..\src\.nuget\nuget.exe pack pkg\Cloudinary.nuspec /BasePath pkg
+    }
+
+    Remove-Item -Recurse pkg
+}
+
+function SetNuSpecVersionInfo {
+    (Get-Content "Cloudinary.nuspec") | Foreach-Object {
+        $_ -replace 'PKG_VERSION', $config.version `
+           -replace 'PKG_RELEASENOTES', $config.releaseNotes
+        } | Set-Content "pkg\Cloudinary.nuspec"
 }
 
