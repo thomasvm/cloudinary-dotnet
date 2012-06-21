1d542fe8e01f722220e566d9316cb220f45c7657
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-06-21 13:05:00 +0200

    Add angle

---
+++ src/Cloudinary/Angle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Cloudinary
+{
+    public class Angle
+    {
+        public static readonly Angle Auto = new Angle("auto");
+        public static readonly Angle AutoLeft = new Angle("auto_left");
+        public static readonly Angle AutoRight = new Angle("auto_right");
+
+        public int? Value { get; private set; }
+
+        public string ValueString { get; private set; }
+        
+        public Angle(int angle)
+        {
+            Value = angle;
+            ValueString = angle.ToString(CultureInfo.InvariantCulture);
+        }
+
+        public Angle(string specialValue)
+        {
+            ValueString = specialValue;
+        }
+
+        public string ToCloudinaryString()
+        {
+            return "a_" + ValueString;
+        }
+    }
+
+}
