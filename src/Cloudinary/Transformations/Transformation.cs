6f20a530b660a0b0d47958d53969857e15debe0d
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2012-06-21 12:55:14 +0200

    Create base transformation without sizing info

--- src/Cloudinary/Transformations/Transformation.cs
+++ src/Cloudinary/Transformations/Transformation.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using Cloudinary.Transformations;
 
 namespace Cloudinary
 {
     /// <summary>
     /// The default Transformation
     /// </summary>
-    public class Transformation : ITransformation
+    public class Transformation : TransformationBase
     {
         /// <summary>
         /// Gets of sets the Width of the image
@@ -21,58 +22,6 @@ namespace Cloudinary
         /// </summary>
         public int Height { get; set; }
 
-        /// <summary>
-        /// Gets of sets Crop mode
-        /// </summary>
-        public CropMode? Crop { get; set; }
-
-        /// <summary>
-        /// Gets of sets Gravity of the tranformation
-        /// </summary>
-        public Gravity? Gravity { get; set; }
-
-        /// <summary>
-        /// Gets or sets the Format of the image
-        /// </summary>
-        public string Format { get; set; }
-
-        /// <summary>
-        /// Gets or sets the border radius, int.MaxValue
-        /// equals a circle
-        /// </summary>
-        public int? Radius { get; set; }
-
-        /// <summary>
-        /// Image to use when the current image isn't available. 
-        /// Format to use: publicid.format, 
-        /// for example: avatar.jpg
-        /// </summary>
-        public string DefaultImage { get; set; }
-
-        /// <summary>
-        /// Gets the X for the fixed cropping coordinate, must 
-        /// be set to SetFixedCroppingPosition
-        /// </summary>
-        public uint? X { get; private set; }
-
-        /// <summary>
-        /// Gets the X for the fixed cropping coordinate,
-        /// be set to SetFixedCroppingPosition
-        /// </summary>
-        public uint? Y { get; private set; }
-
-        /// <summary>
-        /// Sets the fixed cropping position, note that
-        /// the x and y values are against the original image
-        /// </summary>
-        /// <param name="x">the x value</param>
-        /// <param name="y">the y value</param>
-        public void SetFixedCroppingPosition(uint x, uint y)
-        {
-            X = x;
-            Y = y;
-        }
-
         public Transformation(int width, int height)
         {
             Width = width;
@@ -80,44 +29,18 @@ namespace Cloudinary
             Crop = null;
         }
 
-        public string GetFormat()
+        public override string ToCloudinary()
         {
-            if (string.IsNullOrEmpty(Format))
-                return "jpg";
+            string size = GetSize();
 
-            return Format;
+            string baseTransform = base.ToCloudinary();
+
+            if (string.IsNullOrEmpty(baseTransform))
+                return size;
+
+            return size + "," + baseTransform;
         }
-
-        public string ToCloudinary()
-        {
-            var cli = new StringBuilder();
-            cli.AppendFormat(GetSize());
-
-            if (X.HasValue && Y.HasValue)
-                cli.AppendFormat(",x_{0},y_{1}", X.Value, Y.Value);
-
-            if (Crop.HasValue)
-                cli.AppendFormat(",c_{0}", Crop.Value.ToString().ToLowerInvariant());
-
-            if (Gravity.HasValue)
-                cli.AppendFormat(",g_{0}", Gravity.Value.ToString().ToLowerInvariant());
-
-            if (Radius.HasValue)
-            {
-                string urlValue = Radius.Value.ToString(CultureInfo.InvariantCulture);
-
-                if (Radius.Value == int.MaxValue)
-                    urlValue = "max";
-                    
-                cli.AppendFormat(",r_{0}", urlValue);
-            }
-
-            if (!string.IsNullOrEmpty(DefaultImage))
-                cli.AppendFormat(",d_{0}", DefaultImage);
-
-            return cli.ToString();
-        }
-
+       
         protected virtual string GetSize()
         {
             return string.Format("w_{0},h_{1}", Width, Height);
---
+++ src/Cloudinary/Transformations/TransformationBase.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Cloudinary.Transformations
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TransformationBase : ITransformation
+    {
+        /// <summary>
+        /// Gets of sets Crop mode
+        /// </summary>
+        public CropMode? Crop { get; set; }
+
+        /// <summary>
+        /// Gets of sets Gravity of the tranformation
+        /// </summary>
+        public Gravity? Gravity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Format of the image
+        /// </summary>
+        public string Format { get; set; }
+
+        /// <summary>
+        /// Gets or sets the border radius, int.MaxValue
+        /// equals a circle
+        /// </summary>
+        public int? Radius { get; set; }
+
+        /// <summary>
+        /// Image to use when the current image isn't available. 
+        /// Format to use: publicid.format, 
+        /// for example: avatar.jpg
+        /// </summary>
+        public string DefaultImage { get; set; }
+
+        /// <summary>
+        /// Gets the X for the fixed cropping coordinate, must 
+        /// be set to SetFixedCroppingPosition
+        /// </summary>
+        public uint? X { get; private set; }
+
+        /// <summary>
+        /// Gets the X for the fixed cropping coordinate,
+        /// be set to SetFixedCroppingPosition
+        /// </summary>
+        public uint? Y { get; private set; }
+
+        /// <summary>
+        /// Sets the fixed cropping position, note that
+        /// the x and y values are against the original image
+        /// </summary>
+        /// <param name="x">the x value</param>
+        /// <param name="y">the y value</param>
+        public void SetFixedCroppingPosition(uint x, uint y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public string GetFormat()
+        {
+            if (string.IsNullOrEmpty(Format))
+                return "jpg";
+
+            return Format;
+        }
+
+        public virtual string ToCloudinary()
+        {
+            var cli = new StringBuilder();
+
+            if (X.HasValue && Y.HasValue)
+                cli.AppendFormat(",x_{0},y_{1}", X.Value, Y.Value);
+
+            if (Crop.HasValue)
+                cli.AppendFormat(",c_{0}", Crop.Value.ToString().ToLowerInvariant());
+
+            if (Gravity.HasValue)
+                cli.AppendFormat(",g_{0}", Gravity.Value.ToString().ToLowerInvariant());
+
+            if (Radius.HasValue)
+            {
+                string urlValue = Radius.Value.ToString(CultureInfo.InvariantCulture);
+
+                if (Radius.Value == int.MaxValue)
+                    urlValue = "max";
+                    
+                cli.AppendFormat(",r_{0}", urlValue);
+            }
+
+            if (!string.IsNullOrEmpty(DefaultImage))
+                cli.AppendFormat(",d_{0}", DefaultImage);
+
+            return cli.ToString().Trim(',');
+        }
+    }
+}
